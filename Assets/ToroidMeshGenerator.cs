390adc05ea3c47f10cab477801eaa9780284e9f9
Dennis McWilliams <d12768@gmail.com>
2021-05-20 20:44:55 -0400

    1st

--- Assets/ToroidMeshGenerator.cs
+++ Assets/ToroidMeshGenerator.cs
@@ -21,11 +21,8 @@ public class ToroidMeshGenerator : MonoBehaviour
         mesh = new Mesh();
         GetComponent<MeshFilter>().mesh = mesh;
         GetComponent<MeshRenderer>().material = material;
-
-
-        CreateShape(Vector3.zero, 4f, 1f, new Vector3(4,0,0), new Vector3(0,0,4f));
+        CreateShape(new Vector3(0,0,1), 4f, 1f, new Vector3(2.5f,1,1), new Vector3(-1,3,0f));
         UpdateMesh();
-
     }
 
     private void UpdateMesh()
@@ -51,16 +48,21 @@ public class ToroidMeshGenerator : MonoBehaviour
         Plane planeOfStartEndCap = new Plane(Vector3.Cross(start - center, planeOfArc.normal).normalized, start);
         Plane planeofFinishEndCap = new Plane(Vector3.Cross(finish - center, planeOfArc.normal).normalized, finish);
 
+        R = (start - center).magnitude;
+        transform.position = center;
+        transform.rotation = Quaternion.LookRotation(planeOfArc.normal, (start-center));
+        transform.Rotate(new Vector3(0, 0, 90f));
         int st = 15;                    //number of times we draw a ring
         int sl = 15;                    //number of subdivisions of the ring
-        float innerR = 1;               //inner radius
-        float outerR = 2;               //ring radius(thick part of donut)
 
         float phi = 0.0f;
         float dp = (2 * Mathf.PI) / sl; 
         float theta = 0.0f;
-        float dt = (2 * Mathf.PI) / st;
 
+        float angleOfBend = 180f;
+        angleOfBend = Vector3.Angle(finish - center, start - center);
+
+        float dt = (angleOfBend * Mathf.PI/180f) / (st-1);
 
         List<Vector3> vertList = new List<Vector3>();
         for (int stack = 0; stack < st; stack++) //for stack = 0, st do
@@ -69,11 +71,10 @@ public class ToroidMeshGenerator : MonoBehaviour
             for (int slice = 0; slice < sl; slice++)
             {
                 phi = dp * slice;
-                Vector3 v = new Vector3(Mathf.Cos(theta) * (outerR + Mathf.Cos(phi) * innerR), Mathf.Sin(theta) * (outerR + Mathf.Cos(phi) * innerR), Mathf.Sin(phi) * innerR);
+                Vector3 v = new Vector3(Mathf.Cos(theta) * (R + Mathf.Cos(phi) * r), Mathf.Sin(theta) * (R + Mathf.Cos(phi) * r), Mathf.Sin(phi) * r);
                 vertList.Add(v);
             }
         }
-
 
         List<int> triangleList = new List<int>();
         int i1 = 0;
@@ -86,9 +87,9 @@ public class ToroidMeshGenerator : MonoBehaviour
             {
                 if (stack == 0)
                 {
-                    i1 = st-1 + (slice * sl);
+                    i1 = st - 1 + (slice * sl);
                     i2 = (0) + (slice * sl);
-                    i3 = st-1 + ((slice + 1) * sl);
+                    i3 = st - 1 + ((slice + 1) * sl);
                     i4 = (0) + ((slice + 1) * sl);
 
                     triangleList.Add(i1);
@@ -112,29 +113,21 @@ public class ToroidMeshGenerator : MonoBehaviour
 
             }
 
-            i1 = i3;
-            i2 = i4;
-            i3 = stack;
-            i4 = stack + 1;
-            triangleList.Add(i1);
-            triangleList.Add(i3);
-            triangleList.Add(i4);
-            triangleList.Add(i1);
-            triangleList.Add(i4);
-            triangleList.Add(i2);
+            //i1 = i3;
+            //i2 = i4;
+            //i3 = stack;
+            //i4 = stack + 1;
+            //triangleList.Add(i1);
+            //triangleList.Add(i3);
+            //triangleList.Add(i4);
+            //triangleList.Add(i1);
+            //triangleList.Add(i4);
+            //triangleList.Add(i2);
 
         }
-
         vertices = vertList.ToArray();
         triangles = triangleList.ToArray();
         lineRenderer.positionCount = vertList.Count;
         lineRenderer.SetPositions(vertList.ToArray());
-
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
     }
 }
